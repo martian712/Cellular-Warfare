72b2def2223f455a7526fef3c092b3a7e990c419
Brandon Langmeier <blang712@gmail.com>
2015-04-22 17:37:37 -0400

    Added Existing Initial project files
    Camera pans
    Cells, background and walls exist.

---
+++ Assets/Scripts/Camera2D.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class Camera2D : MonoBehaviour
+{
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += new Vector3(speed * Time.deltaTime,0,0);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += new Vector3(-speed * Time.deltaTime,0,0);
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += new Vector3(0,-speed * Time.deltaTime,0);
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += new Vector3(0,speed * Time.deltaTime,0);
+		}
+	}
+}
+
